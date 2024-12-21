a1eee015fb0fc30762f8914aad0d64a75f2a4b4d
Gabriel Matheus <gabriel65451@gmail.com>
2024-12-20 21:53:42 -0300

    add items

--- ADM/appadm.js
+++ ADM/appadm.js
@@ -1,25 +1,92 @@
 
 
-function pegaInfos() {
-var dados = []
-
+function cadastrarItem() {
+	
 	var nomeProd = document.getElementById('add-prod-name').value;
 	var valorProd = document.getElementById('add-prod-valor').value;
 	var descricaoProd = document.getElementById('add-prod-descricao').value;
 	var categoriaProd = document.getElementById('add-prod-categoria').value;
-
-	if(nomeProd !== "" && valorProd !== "" && descricaoProd !== "" && categoriaProd !== ""){
-
-	return dados.push({produto : nomeProd, valorProd : valorProd, descricao : descricaoProd});
-
-	} else {
-		alert('asdas')
+	
+	var valorNumber = Number(valorProd)
+	
+	var produtosCadastrado = {
+		nome : nomeProd,
+		valor : valorNumber,
+		descricao : descricaoProd,
+		categoria : categoriaProd
 	}
 
 
-	var dadoString = JSON.stringify(dados);
-	
-	let x = 0;
-	
-	localStorage.setItem(x + 1, dadoString)
+	if( nomeProd.length >= 3 ){
+		 if( valorNumber !== 0){
+			if( descricaoProd.length >= 5){
+				if( categoriaProd !== 'notSelect'){
+					
+					var produtosString =  JSON.stringify(produtosCadastrado);
+
+                    return localStorage.setItem(localStorage.length + 1, produtosString);
+					
+				} else {
+					alert('Adicionar uma categoria para o produto!')
+				}
+
+			} else {
+				alert('Adicionar uma descrição para o produto!')
+			}
+
+		 } else {
+			alert('Adicionar um valor para o produto!')
+		 }
+		 
+	} else {
+		alert('Adicionar um nome para o produto!')
+	}
+
+
 }
+
+
+
+
+function itenSalvo() {
+
+    let produtosExibir = []
+    
+
+    let prodQtd =  localStorage.length;
+
+    for(i = 1; i <= prodQtd; i++) {
+
+        let prodExibir = localStorage.getItem(i);
+        
+        let prodObj = JSON.parse(prodExibir);
+
+        produtosExibir.push(prodObj);
+        
+        let template = `
+    
+        <div class="cards">
+        
+          <img src="../../imgs/produtos/sobremesa.png" alt="pudim de chocolarte" class="imgs-cards">
+          <h3>${prodObj.nome}</h3>
+          <div class="descricao-cards">
+            <p>${prodObj.descricao}</p>
+          </div>
+
+          <div class="btn-produtos">
+            <button>Editar</button>
+            <button>Excluir</button>
+          </div>$
+        </div>
+        `;
+
+        
+		document.querySelector('#produtos-salvos').innerHTML += template;
+		
+    }
+
+}
+
+
+
+window.onload = itenSalvo();
